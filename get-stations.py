263028a4437261e81e3aa23487f7018414ec0a11
Andrzej Wójtowicz <andre@amu.edu.pl>
2017-09-09 17:22:49 +0200

    Added Tor and random traverse on stations list

--- get-stations.py
+++ get-stations.py
@@ -1,15 +1,26 @@
 #!/usr/bin/python3
 
-# run as PYTHONIOENCODING=utf-8 python3 -i get-stations.py
+# usage: PYTHONIOENCODING=utf-8 TOR_CONTROL_PASSWORD=p@s$w0rd python3 get-stations.py
+
+# TODO: 
+# - add lock execution
+
+# ------- libraries -------------------------------------------------
 
 import datetime
 import json
 import logging
 import os
+import random
 import requests
 import sys
 import tempfile
 import zipfile
+
+from stem import Signal
+from stem.control import Controller
+
+# ------- config -------------------------------------------------
 
 SCRIPT_RUN_DATE_TIME   = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
 
@@ -38,7 +49,6 @@ HEADER_ACCEPT          = "application/json, text/plain, */*"
 HEADER_ACCEPT_ENCODING = "gzip, deflate"
 HEADER_ACCEPT_LANGUAGE = "pl-PL"
 HEADER_CONNECTION      = "Keep-Alive"
-#HEADER_CONTENT_LENGTH  = "0"
 HEADER_HOST            = "api.nextbike.net"
 HEADER_ORIGIN          = "https://webview.nextbike.net"
 HEADER_REFERER         = "https://webview.nextbike.net/"
@@ -48,123 +58,158 @@ HEADER_CONTENT_TYPE    = "Content-Type: application/x-www-form-urlencoded; chars
 
 HTTP_STATUS_CODE_OK    = 200
 
+TOR_CONTROL_PORT       = 9051
+TOR_CONTROL_PASSWORD   = os.environ.get("TOR_CONTROL_PASSWORD")
+TOR_SOCKS_ADDRESS      = "socks5://127.0.0.1:9050"
+
+# ------- functions -------------------------------------------------
+
+def renew_tor_connection(tc_port = 9051, tc_password = "password"):
+    with Controller.from_port(port = tc_port) as controller:
+        controller.authenticate(password = tc_password)
+        controller.signal(Signal.NEWNYM)
+
+def get_tor_session(t_address = "socks5://127.0.0.1:9050"):
+    session = requests.session()
+    session.proxies = {"http":  t_address,
+                       "https": t_address}
+    return session
+    
+def get_logger(l_filename, l_encoding, l_fmt, l_datefmt, l_level):
+    logging_handler = logging.FileHandler(filename = LOGGING_FILENAME, 
+                                          encoding = LOGGING_ENCODING)
+    logging_formatter = logging.Formatter(fmt      = LOGGING_FORMAT,
+                                          datefmt  = LOGGING_DATE_FORMAT)
+    logging_handler.setFormatter(logging_formatter)
+    logging_handler.setLevel(LOGGING_LEVEL)
+
+    logger = logging.getLogger()
+    logger.addHandler(logging_handler)
+    
+    return logger
+
 # -------------------------------------------------------------------
+    
+if __name__ == "__main__":
 
-logging_handler = logging.FileHandler(filename = LOGGING_FILENAME, 
-                                      encoding = LOGGING_ENCODING)
-logging_formatter = logging.Formatter(fmt      = LOGGING_FORMAT,
-                                      datefmt  = LOGGING_DATE_FORMAT)
-logging_handler.setFormatter(logging_formatter)
-logging_handler.setLevel(LOGGING_LEVEL)
-
-logger = logging.getLogger()
-logger.addHandler(logging_handler)
-
-# -------------------------------------------------------------------
-
-if os.environ.get("PYTHONIOENCODING") != "utf-8":
-    logger.error("Run script with system env PYTHONIOENCODING=utf-8")
-    sys.exit(1)
-
-# TODO: add proxy
-
-if not os.path.isdir(DATA_DIR):
-    os.mkdir(DATA_DIR)
-
-try:
-    sl_r = requests.post(
-                url = URL_STATIONS_LIST.format(DATA_LAT, DATA_LNG, DATA_DISTANCE), 
-                headers = { "Accept" :          HEADER_ACCEPT,
-                            "Accept-Encoding" : HEADER_ACCEPT_ENCODING,
-                            "Accept-Language" : HEADER_ACCEPT_LANGUAGE,
-                            "Connection" :      HEADER_CONNECTION,
-                            #"Content-Length" :  HEADER_CONTENT_LENGTH,
-                            "Host" :            HEADER_HOST,
-                            "Origin" :          HEADER_ORIGIN,
-                            "Referer" :         HEADER_REFERER,
-                            "User-Agent" :      HEADER_USER_AGENT }
-            )
-except Exception as e:
-    logger.error(e.strerror)
-    sys.exit(1)
-
-if sl_r.status_code == HTTP_STATUS_CODE_OK:
-
-    if len(sl_r.text) == 0:
-        logger.error("Stations list empty text")
+    logger = get_logger(LOGGING_FILENAME, LOGGING_ENCODING, LOGGING_FORMAT, LOGGING_DATE_FORMAT, LOGGING_LEVEL)
+    
+    if os.environ.get("PYTHONIOENCODING") != "utf-8":
+        logger.error("Run script with system env PYTHONIOENCODING=utf-8")
         sys.exit(1)
-
+        
+    if len(os.environ.get("TOR_CONTROL_PASSWORD")) == 0:
+        logger.error("Run script with system env TOR_CONTROL_PASSWORD=p@s$w0rd")
+        sys.exit(1)
+        
+    if not os.path.isdir(DATA_DIR):
+        os.mkdir(DATA_DIR)
+        
     try:
-        sl_data = sl_r.json()
+        renew_tor_connection(TOR_CONTROL_PORT, TOR_CONTROL_PASSWORD)
+        session = get_tor_session(TOR_SOCKS_ADDRESS)
     except Exception as e:
         logger.error(e.strerror)
         sys.exit(1)
 
-    tmp = tempfile.mkstemp()
-    f = open(tmp[1], "w")
-    json.dump(sl_r.json(), open(f.name, "w", encoding = "utf-8"), ensure_ascii = False)
-    f.close()
-    sl_zf = zipfile.ZipFile(os.path.join(DATA_DIR, ZIP_STATIONS_LIST), "w", zipfile.ZIP_DEFLATED)
-    sl_zf.write(f.name, JSON_STATIONS_LIST)
-    sl_zf.close()
-    os.unlink(f.name)
-    
-    bl_zf = zipfile.ZipFile(os.path.join(DATA_DIR, ZIP_BIKES_LIST), "w", zipfile.ZIP_DEFLATED)  
-    
-    if "countries" not in sl_data.keys():
-        logger.error("Stations list no countries")
+    try:
+        sl_r = session.post(
+                    url = URL_STATIONS_LIST.format(DATA_LAT, DATA_LNG, DATA_DISTANCE), 
+                    headers = { "Accept" :          HEADER_ACCEPT,
+                                "Accept-Encoding" : HEADER_ACCEPT_ENCODING,
+                                "Accept-Language" : HEADER_ACCEPT_LANGUAGE,
+                                "Connection" :      HEADER_CONNECTION,
+                                "Host" :            HEADER_HOST,
+                                "Origin" :          HEADER_ORIGIN,
+                                "Referer" :         HEADER_REFERER,
+                                "User-Agent" :      HEADER_USER_AGENT }
+                )
+    except Exception as e:
+        logger.error(e.strerror)
         sys.exit(1)
-    
-    for country_data in sl_data["countries"]:
+
+    if sl_r.status_code == HTTP_STATUS_CODE_OK:
+
+        if len(sl_r.text) == 0:
+            logger.error("Stations list empty text")
+            sys.exit(1)
+
+        try:
+            sl_data = sl_r.json()
+        except Exception as e:
+            logger.error(e.strerror)
+            sys.exit(1)
+
+        tmp = tempfile.mkstemp()
+        f = open(tmp[1], "w")
+        json.dump(sl_r.json(), open(f.name, "w", encoding = "utf-8"), ensure_ascii = False)
+        f.close()
+        sl_zf = zipfile.ZipFile(os.path.join(DATA_DIR, ZIP_STATIONS_LIST), "w", zipfile.ZIP_DEFLATED)
+        sl_zf.write(f.name, JSON_STATIONS_LIST)
+        sl_zf.close()
+        os.unlink(f.name)
         
-        if "cities" not in country_data.keys():
-            logger.error("Stations list no cities")
+        bl_zf = zipfile.ZipFile(os.path.join(DATA_DIR, ZIP_BIKES_LIST), "w", zipfile.ZIP_DEFLATED)  
+        
+        if "countries" not in sl_data.keys():
+            logger.error("Stations list no countries")
             sys.exit(1)
         
-        for city_data in country_data["cities"]:
+        for country_data in sl_data["countries"]:
             
-            if "places" not in city_data.keys():
-                logger.error("Stations list no places")
+            if "cities" not in country_data.keys():
+                logger.error("Stations list no cities")
                 sys.exit(1)
             
-            for place_data in city_data["places"]:
+            for city_data in country_data["cities"]:
                 
-                if "number" not in place_data.keys():
-                    logger.error("Stations list no number")
+                if "places" not in city_data.keys():
+                    logger.error("Stations list no places")
                     sys.exit(1)
                 
-                try:
-                    bl_r = requests.post(
-                        url = URL_BIKE_LIST.format(DATA_APIKEY, place_data["uid"], DATA_SHOW_ERRORS),
-                        headers = { "Accept" :          HEADER_ACCEPT,
-                                    "Accept-Encoding" : HEADER_ACCEPT_ENCODING,
-                                    "Accept-Language" : HEADER_ACCEPT_LANGUAGE,
-                                    "Connection" :      HEADER_CONNECTION,
-                                    "Content-Type" :    HEADER_CONTENT_TYPE,
-                                    #"Content-Length" :  HEADER_CONTENT_LENGTH,
-                                    "Host" :            HEADER_HOST,
-                                    "Origin" :          HEADER_ORIGIN,
-                                    "Referer" :         HEADER_REFERER,
-                                    "User-Agent" :      HEADER_USER_AGENT }
-                    )
-                except Exception as e:
-                    logger.error(e.strerror)
-                    sys.exit(1)
-                
-                if bl_r.status_code == HTTP_STATUS_CODE_OK:
+                for place_data in random.shuffle(city_data["places"]):
                     
-                    if len(bl_r.text) == 0:
-                        logger.warning("Bikes list [{0} - {1} - {2} - {3}] empty text".format(country_data["country_name"], city_data["name"], place_data["name"], place_data["number"]))
+                    if "number" not in place_data.keys():
+                        logger.warning("Stations list no number")
+                        continue
+                        
+                    try:
+                        renew_tor_connection(TOR_CONTROL_PORT, TOR_CONTROL_PASSWORD)
+                        session = get_tor_session(TOR_SOCKS_ADDRESS)
+                    except Exception as e:
+                        logger.warning(e.strerror)
+                        continue
+                    
+                    try:
+                        bl_r = session.post(
+                            url = URL_BIKE_LIST.format(DATA_APIKEY, place_data["uid"], DATA_SHOW_ERRORS),
+                            headers = { "Accept" :          HEADER_ACCEPT,
+                                        "Accept-Encoding" : HEADER_ACCEPT_ENCODING,
+                                        "Accept-Language" : HEADER_ACCEPT_LANGUAGE,
+                                        "Connection" :      HEADER_CONNECTION,
+                                        "Content-Type" :    HEADER_CONTENT_TYPE,
+                                        "Host" :            HEADER_HOST,
+                                        "Origin" :          HEADER_ORIGIN,
+                                        "Referer" :         HEADER_REFERER,
+                                        "User-Agent" :      HEADER_USER_AGENT }
+                        )
+                    except Exception as e:
+                        logger.error(e.strerror)
                         sys.exit(1)
                     
-                    bl_zf.writestr(XML_BIKES_LIST.format(place_data["uid"]), bl_r.text)
-                    
-                else:
-                    logger.warning("Bikes list [{0} - {1} - {2} - {3}] status code {4}".format(country_data["country_name"], city_data["name"], place_data["name"], place_data["number"], bl_r.status_code))
-        
-    bl_zf.close()
-                    
-else:
-    logger.error("Stations list status code {0}".format(sl_r.status_code))
-    sys.exit(1)
-    
+                    if bl_r.status_code == HTTP_STATUS_CODE_OK:
+                        
+                        if len(bl_r.text) == 0:
+                            logger.warning("Bikes list [{0} - {1} - {2} - {3}] empty text".format(country_data["country_name"], city_data["name"], place_data["name"], place_data["number"]))
+                            sys.exit(1)
+                        
+                        bl_zf.writestr(XML_BIKES_LIST.format(place_data["uid"]), bl_r.text)
+                        
+                    else:
+                        logger.warning("Bikes list [{0} - {1} - {2} - {3}] status code {4}".format(country_data["country_name"], city_data["name"], place_data["name"], place_data["number"], bl_r.status_code))
+            
+        bl_zf.close()
+                        
+    else:
+        logger.error("Stations list status code {0}".format(sl_r.status_code))
+        sys.exit(1)
